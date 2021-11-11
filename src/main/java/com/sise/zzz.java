469cee766bbed324c08c072223a5341c5536580f
Architect <1359944187@qq.com>
2021-11-11 13:36:08 +0800

    second commit

---
+++ src/main/java/com/sise/zzz.java
@@ -0,0 +1,11 @@
+package com.sise;
+
+/**
+ * @author chen
+ * @create 2021/11/11-13:34
+ */
+public class zzz {
+    public static void main(String[] args) {
+        System.out.println("zzz");
+    }
+}
