595eb73e2ff9774e267706163ff5749d3363dc2b
Architect <1359944187@qq.com>
2021-11-11 13:28:01 +0800

    first commit

---
+++ src/main/java/com/sise/GitTest.java
@@ -0,0 +1,11 @@
+package com.sise;
+
+/**
+ * @author chen
+ * @create 2021/11/11-13:23
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
